a61ae2fc9f22f10103fff35098e3a2aa0e634d17
beto flores luis <titinrex@hotmail.com>
2018-04-27 22:44:41 -0500

    diccionario

---
+++ practica7.py
@@ -0,0 +1,6 @@
+diccionario = {
+    "lenguajes": ["python", "java", "R", "go", "Swift"],
+    "shakespeare": ["Hamlet", "Macbeth", "Romeo y Julieta", "La tempestad"],
+    "esto_es_un_diccionario": True
+
+    }
